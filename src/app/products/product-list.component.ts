4c0fcaf677dfb60d45c43210ab7ca81ebfedb1f3
Fred Morrison <fpmorrison@verizon.net>
2018-02-24 10:57:58 -0500

    property bag service used to retain showImage and filterBy settings

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -30,6 +30,8 @@ export class ProductListComponent implements OnInit, AfterViewInit {
   filteredProducts: IProduct[];
   products: IProduct[];
 
+
+
   get showImage(): boolean {
     return this.productParameterService.showImage;
   }
@@ -37,6 +39,7 @@ export class ProductListComponent implements OnInit, AfterViewInit {
   set showImage(value: boolean) {
     this.productParameterService.showImage = value;
   }
+
   constructor(private productService: ProductService,
               private productParameterService: ProductParamterService) {
   }
@@ -49,13 +52,14 @@ export class ProductListComponent implements OnInit, AfterViewInit {
     this.productService.getProducts().subscribe(
       (products: IProduct[]) => {
         this.products = products;
-        this.performFilter(this.parentListFilter);
+        this.filterComponent.listFilter = this.productParameterService.filterBy;
       },
       (error: any) => this.errorMessage = <any>error
     );
   }
 
   onValueChange(value: string) : void {
+    this.productParameterService.filterBy = value;
     this.performFilter(value);
   }
 
--- src/app/products/product-paramter.service.ts
+++ src/app/products/product-paramter.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ProductParamterService {
   showImage: boolean;
-  filerBy: string;
+  filterBy: string;
 
   constructor() { }
 
